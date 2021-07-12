f5c6561b91f9ee7a9a21bc1d48946b14e14f4281
jihoKim94 <kj941225@gmail.com>
2021-07-12 21:25:38 +0900

    날씨앱 프로젝트 생성

---
+++ ForecastAppByJiho/ForecastAppByJiho/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForecastAppByJiho
+//
+//  Created by 김지호 on 2021/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
